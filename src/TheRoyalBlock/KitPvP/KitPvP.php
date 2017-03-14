8e5a7f57d3ff568eb68b7cd54941275711e0ddcf
TheRoyalBlock <turbinenetwork@gmail.com>
2017-03-14 10:42:09 -0400

    Update KitPvP.php

--- src/TheRoyalBlock/KitPvP/KitPvP.php
+++ src/TheRoyalBlock/KitPvP/KitPvP.php
@@ -1,19 +1,57 @@
 <?php
 namespace TheRoyalBlock\KitPvP;
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\command\Command;
+//Blocks
+use pocketmine\block\Block;
+//Command
 use pocketmine\command\CommandSender;
-use pocketmine\utils\Config;
+use pocketmine\command\Command;
+//Entity
+use pocketmine\entity\Entity;
 use pocketmine\entity\Effect;
-use pocketmine\item\item;
 //Events
-use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent; 
+//Inventory
+use pocketmine\inventory\ChestInventory;
+use pocketmine\inventory\EnderChestInventory;
+//Item
+use pocketmine\item\Item;
+//Lang
+//Level
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+//Math
+use pocketmine\math\Vector3;
+//Metadata
+//Nbt
+use pocketmine\nbt\NBT;
+//Network
+use pocketmine\network\Network;
+//Permission
+use pocketmine\permission\Permission;
+//Plugin
+use pocketmine\plugin\PluginBase;
+//Scheduler
+use pocketmine\scheduler\PluginTask;
+//Tile
+use pocketmine\tile\Sign;
+use pocketmine\tile\Chest;
+//Utils
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+//Other
+use pocketmine\Player;
+use pocketmine\Server;
+
 class KitPvP extends PluginBase implements Listener {
     public $prefix = "§7[§cKitPvP§7] §f";
 //=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
